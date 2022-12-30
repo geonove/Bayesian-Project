6171fec22b5f5c14f1b39f48a2531de31422cb56
andrespasinetti <10468985@polimi.it>
2022-12-30 01:03:53 +0100

    added 3d plot option

--- GS_DAGS.R
+++ GS_DAGS.R
@@ -4,59 +4,67 @@ library(BCDAG)
 library(gss)
 library(graph)
 library(Rcpp)
+library(rgl)
+library(car)
 
-setwd("C:/Users/andre/Documents/Università/Magistrale/2° Anno/1° Semestre/Bayesian statistics/Progetto/codes")
+#setwd("C:/Users/andre/Documents/UniversitÃ /Magistrale/2Â° Anno/1Â° Semestre/Bayesian statistics/Progetto/codes")
+setwd("/Users/andrespasinetti/Bayesian-Project-4")
+
 source("dags/propose_DAG.R")
 source("dags/operation.R")
 source("dags/acceptreject_DAG.R")
 source("dags/new_bcdag.R")
 source("dags/update_DAG.R")
 
-
 ### Plot 3d
-#library(car)
-
-#scatter3d(x = x[, 1], y = x[, 2], z = x[, 3], point.col= z_real, surface = FALSE)
 #install.packages(c("rgl", "car"))
 
 # PARAMETERS
-C <- 4 # number of classes
+C <- 3 # number of classes
 alpha_0 <- runif(n = C, min = 1, max = 4)
-q <- 2
-N_SAMPLES <- 200 # number of samples
-niter <- 300 # number of Gibbs Sampling iterations
+q <- 3
+N_SAMPLES <- 700 # number of samples
+niter <- 20 # number of Gibbs Sampling iterations
 
 w_real <- gtools::rdirichlet(1, alpha_0)
-DAG <- array(dim = c(q, q, C))
-Omega <- array(dim = c(q, q, C))
-for (i in 1:C) {    
-    DAG[, , i] <- rDAG(q = q, w = 0.5)
-    L <- matrix(runif(n = q*(q-1), min = 0.1, max = 1), q, q)     ### va bene mettere questi min e max? 
-    L <- L * DAG[, , i] 
+DAG_real <- array(dim = c(q, q, C))
+Omega_real <- array(dim = c(q, q, C))
+for (c in 1:C) {    
+    DAG_real[, , c] <- rDAG(q = q, w = 0.5)
+    L <- matrix(runif(n = q*(q-1), min = -10, max = 10), q, q)     ### va bene mettere questi min e max? 
+    L <- L * DAG_real[, , c] 
     diag(L) <- 1
     D <- diag(1, q)
-    Omega[, , i] <- L%*%solve(D)%*%t(L)       
+    Omega_real[, , c] <- L%*%solve(D)%*%t(L)       
 }
 
 
-rmix <- function(w_real, Omega) {
+rmix <- function(w_real, Omega_real) {
     z_real <- sample(1:C, prob = w_real, size = N_SAMPLES, replace = TRUE)
     x <- matrix(, nrow = N_SAMPLES, ncol = q)
     for (i in 1:N_SAMPLES) {
-        x[i, ] <- mvtnorm::rmvnorm(1, sigma=solve(Omega[, ,z_real[i]]))
+        x[i, ] <- mvtnorm::rmvnorm(1, sigma=solve(Omega_real[, ,z_real[i]]))
     }
   
   return(list(x, z_real))
 }
 
-mix <- rmix(w_real, Omega)
+mix <- rmix(w_real, Omega_real)
 x <- mix[1]
 z_real <- mix[2]
 
 x <- data.frame(x)
 z_real <- unlist(z_real)
-x11()
-plot(x, type="p", col=z_real, pch=20)
+if (q==2) {
+  x11()
+  plot(x, type="p", col=z_real, pch=20)
+} else if (q==3) {
+  open3d()
+  scatter3d(x = x[, 1], y = x[, 2], z = x[, 3], point.col= z_real, surface = FALSE)
+
+}
+
+
 
 # Full conditionals
 sample_w <- function(alpha_0, N) {
@@ -98,13 +106,13 @@ gibbs <- function(x, niter, C, alpha_0) {
   # Initialize DAG
   DAG <- array(dim = c(q, q, C))
   Omega <- array(dim = c(q, q, C))
-  for (i in 1:C) {    
-    DAG[, , i] <- rDAG(q = q, w = w_dags)
+  for (c in 1:C) {    
+    DAG[, , c] <- rDAG(q = q, w = w_dags)
     L <- matrix(runif(n = q*(q-1), min = -10, max = 10), q, q)     ### va bene mettere questi min e max? 
-    L <- L * DAG[, , i] 
+    L <- L * DAG[, , c] 
     diag(L) <- 1
     D <- diag(1, q)
-    Omega[, , i] <- L%*%solve(D)%*%t(L)       
+    Omega[, , c] <- L%*%solve(D)%*%t(L)       
   }
   
  
@@ -136,7 +144,7 @@ gibbs <- function(x, niter, C, alpha_0) {
   cat("\nGibbs Sampling\n")
   cat(0, "/", niter, "\n")
   for (i in 1:niter) {
-    if (i %% 10 == 0) {
+    if (i %% 5 == 0) {
       cat(i, "/", niter, "\n")
     }
 
@@ -150,7 +158,7 @@ gibbs <- function(x, niter, C, alpha_0) {
     z <- sample_z(mu, Omega, w, x)
     out <- update_DAGS(DAG = DAG, data = x, z = z, a = q, U=diag(1, q), w = w_dags)
     DAG <- out$DAG
-    #Omega <- out$Omega
+    Omega <- out$Omega
 
     w_GS[, i] <- w
     z_GS[, i] <- z
@@ -161,14 +169,28 @@ gibbs <- function(x, niter, C, alpha_0) {
   return(list("w_GS" = w_GS, "z_GS" = z_GS, "DAG_GS" = DAG_GS, "Omega_GS" = Omega_GS))
 }
 
-mix <- gibbs(x, 100, C, alpha_0)
+mix <- gibbs(x, niter, C, alpha_0)
 
 
+if (q==2) {
+  x11()
+  plot(x, type="p", col=mix$z_GS[, niter], pch=20)
+} else if (q==3) {
+  open3d()
+  x <- data.frame(x)
+  scatter3d(x = x[, 1], y = x[, 2], z = x[, 3], point.col= mix$z_GS[, niter], surface = FALSE)
 
-x11()
-plot(x, type="p", col=mix$z_GS[, 100], pch=20)
+}
 
 
+cat("\nDAG_real:\n")
+print(DAG_real)
+cat("\nDAG_real:\n")
+print(mix$DAG_GS[, , , niter])
+cat("\nOmega_real:\n")
+print(Omega_real)
+cat("\nOmega:\n")
+print(mix$Omega_GS[, , , niter])
 
 
-
+#x <- data.frame(x)
